0abab74124569b9c0eaaad84975e8c1d086e1304
Marijn Regterschot <marijn.regterschot@gmail.com>
2024-09-25 23:04:37 +0200

    fix maplikes

--- core/plugins/widgets/maplikes/index.ts
+++ core/plugins/widgets/maplikes/index.ts
@@ -1,33 +1,20 @@
 import Plugin from "../../../plugins";
 import Widget from '../../../ui/widget';
 import type { Like } from "../../../plugins/maplikes";
-import type { Player } from "../../../playermanager";
 
 export default class MapLikesWidget extends Plugin {
     static depends: string[] = ["database", 'maplikes'];
-    widgets: { [key: string]: Widget } = {};
-    mapLikes: Like[] = [];
+    widget: Widget | null = null;
+    records: any[] = [];
 
     async onLoad() {
-        tmc.server.addListener("TMC.PlayerConnect", this.onPlayerConnect, this);
-        tmc.server.addListener("TMC.PlayerDisconnect", this.onPlayerDisconnect, this);
         tmc.server.addListener("Plugin.MapLikes.onSync", this.onSync, this);
+        this.widget = new Widget("core/plugins/widgets/maplikes/widget.twig");      
+        this.widget.pos = { x: 115, y: 60 };   
+        this.widget.actions['like'] = tmc.ui.addAction(this.actionLike.bind(this), 1);
+        this.widget.actions['dislike'] = tmc.ui.addAction(this.actionLike.bind(this), -1);
     };
-
-    async onPlayerConnect(player: Player) {
-        const login = player.login;
-        await this.updateWidget(login);
-        if (this.widgets[login]) {
-            await tmc.ui.displayManialink(this.widgets[login]);
-        }
-    }
     
-    async onPlayerDisconnect(player: Player) {
-        const login = player.login;
-        if (this.widgets[login]) {
-            delete this.widgets[login];
-        }
-    }
 
     async actionLike(login: string, value: number) {
         if (value > 0)
@@ -37,57 +24,36 @@ export default class MapLikesWidget extends Plugin {
     }
 
     async onUnload() {
-        for (const login of Object.keys(this.widgets)) {
-            delete this.widgets[login];
-        }
-        tmc.server.addListener("TMC.PlayerConnect", this.onPlayerConnect, this);
-        tmc.server.addListener("TMC.PlayerDisconnect", this.onPlayerDisconnect, this);
         tmc.server.removeListener("Plugin.MapLikes.onSync", this.onSync);
+        this.widget?.destroy();
+        this.widget = null;
     }
 
     async onSync(data: Like[]) {
-        this.mapLikes = data;
-        await this.updateWidgets();
+        await this.updateWidget(data);
     }
 
-    async updateWidgets() {
-        for (const player of tmc.players.getAll()) {
-            await this.updateWidget(player.login);
-        }
-        await tmc.ui.displayManialinks(Object.values(this.widgets));
-    }
+    async updateWidget(data: Like[]) {
+        if (this.widget) {
+            let positive = 0;
+            let total = 0.0001;
 
-    async updateWidget(login: string) {
-        let widget = this.widgets[login];
-        
-        if (!widget) {
-            widget = new Widget("core/plugins/widgets/maplikes/widget.twig");      
-            widget.pos = { x: 115, y: 60 };   
-            widget.actions['like'] = tmc.ui.addAction(this.actionLike.bind(this), 1);
-            widget.actions['dislike'] = tmc.ui.addAction(this.actionLike.bind(this), -1);
-        }
-
-        let positive = 0;
-        let total = 0.0001;
-
-        for (const like of this.mapLikes) {
-            if (like.vote > 0) {
-                positive++;
+            for (const like of data) {
+                if (like.vote > 0) {
+                    positive++;
+                }
+                total++;
             }
-            total++;
+            let percentage = ((positive / total * 100).toFixed(0) || 0) + "%";
+            if (total < 1) percentage = "No Votes";
+            this.widget.setData({
+                percentage: percentage,
+                width: (positive / total * 30).toFixed(0)
+            });
+            this.widget.title = "Map Likes ["+data.length+"]";
+            this.widget.size = { width: 45, height: 6 };
+            await this.widget.display();
         }
-        let percentage = ((positive / total * 100).toFixed(0) || 0) + "%";
-        if (total < 1) percentage = "No Votes";
-
-        widget.setData({
-            percentage: percentage,
-            width: (positive / total * 30).toFixed(0),
-            mapLike: this.mapLikes.find(like => like.login === login)
-        });
-
-        widget.title = "Map Likes ["+this.mapLikes.length+"]";
-        widget.size = { width: 45, height: 6 };
-
-        this.widgets[login] = widget;
     }
+
 }
