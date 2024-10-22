d128746749de00092530607fdc41be72b213e554
MRegterschot <marijn.regterschot@gmail.com>
2024-10-22 17:05:50 +0200

    update logs for multiple types

--- userdata/plugins/log/index.ts
+++ userdata/plugins/log/index.ts
@@ -1,36 +1,43 @@
 import Plugin from "@core/plugins";
 import fs from "fs";
 
+type LogTypes = "messages" | "maps";
+
+const logTypes: LogTypes[] = ["messages", "maps"];
+
 export default class LogPlugin extends Plugin {
 	static depends: string[] = [];
+	private logPath: string = "/../../../logs";
 
 	async onLoad() {
-		tmc.server.addListener("Plugin.Log.message", this.onMessage, this);
+		for (const type of logTypes) {
+			if (!fs.existsSync(import.meta.dirname + this.logPath + `/${type}`)) {
+				fs.mkdirSync(import.meta.dirname + this.logPath + `/${type}`, { recursive: true });
+			}
+		}	
+
+		tmc.server.addListener("Plugin.Log", this.onLog, this);
 	}
 
 	async onUnload() {
-		tmc.server.removeListener("Plugin.Log.message", this.onMessage);
+		tmc.server.removeListener("Plugin.Log", this.onLog);
 	}
 
 	async onStart() {
 
 	}
 
-	async onMessage(data: any) {
+	async onLog(type: LogTypes, data: any) {
 		const currentDate = new Date();
 		const date = currentDate.getFullYear() + '-' +
 			String(currentDate.getMonth() + 1).padStart(2, '0') + '-' +
 			String(currentDate.getDate()).padStart(2, '0');
 
-		if (!fs.existsSync(import.meta.dirname + "/../../../logs/messages")) {
-			fs.mkdirSync(import.meta.dirname + "/../../../logs/messages", { recursive: true });
-		}
-
-		let buffer = fs.readFileSync(`${import.meta.dirname}/../../../logs/messages/${date}.json`, { flag: 'a+' });
+		let buffer = fs.readFileSync(import.meta.dirname + this.logPath + `/${type}/${date}.json`, { flag: 'a+' });
 		let logs = JSON.parse(buffer.toString() || "[]");
 
 		logs.push(data);
 
-		fs.writeFileSync(`${import.meta.dirname}/../../../logs/messages/${date}.json`, JSON.stringify(logs));
+		fs.writeFileSync(import.meta.dirname + this.logPath + `/${type}/${date}.json`, JSON.stringify(logs));
 	}
 }
